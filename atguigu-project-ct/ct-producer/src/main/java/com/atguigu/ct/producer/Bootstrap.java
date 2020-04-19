8cf6ebceede91d1d09c5cac179faebcb337075d9
冯文涛 <fengwentao@yadingdata.com>
2020-04-19 20:50:40 +0800

    modify Bootstrap

--- atguigu-project-ct/ct-producer/src/main/java/com/atguigu/ct/producer/Bootstrap.java
+++ atguigu-project-ct/ct-producer/src/main/java/com/atguigu/ct/producer/Bootstrap.java
@@ -17,7 +17,14 @@ public class Bootstrap {
 //        producer.setIn(new LocalFileDataIn("E:\\data\\baiduyundownload\\尚学堂\\01\\2.资料\\辅助文档\\contact.log"));
 //        producer.setOut(new LocalFileDataOut("E:\\data\\baiduyundownload\\尚学堂\\01\\2.资料\\辅助文档\\call.log"));
 
+        if(args.length < 2){
+            System.err.println("请输入正确格式的参数：\n参数1：联系人路径 \n参数2：通话记录输出路径");
+            System.exit(1);
+        }
 
+
+        producer.setIn(new LocalFileDataIn(args[0]));
+        producer.setOut(new LocalFileDataOut(args[1]));
         //生产数据
         producer.produce();
         //关闭生产者对象
