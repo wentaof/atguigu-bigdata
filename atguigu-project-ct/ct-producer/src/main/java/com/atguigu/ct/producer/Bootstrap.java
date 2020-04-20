cba3ebafe304629e4925c009f268fe351f59537b
冯文涛 <fengwentao@yadingdata.com>
2020-04-20 16:38:59 +0800

    add office env

--- atguigu-project-ct/ct-producer/src/main/java/com/atguigu/ct/producer/Bootstrap.java
+++ atguigu-project-ct/ct-producer/src/main/java/com/atguigu/ct/producer/Bootstrap.java
@@ -14,8 +14,8 @@ public class Bootstrap {
     public static void main(String[] args) throws IOException {
         //构建生产者对象
         Producer producer = new LocalFileProducer();
-//        producer.setIn(new LocalFileDataIn("E:\\data\\baiduyundownload\\尚学堂\\01\\2.资料\\辅助文档\\contact.log"));
-//        producer.setOut(new LocalFileDataOut("E:\\data\\baiduyundownload\\尚学堂\\01\\2.资料\\辅助文档\\call.log"));
+//        producer.setIn(new LocalFileDataIn("D:\\coder\\mylearn\\files\\2.资料\\辅助文档\\contact.log"));
+//        producer.setOut(new LocalFileDataOut("D:\\coder\\mylearn\\files\\2.资料\\辅助文档\\call.log"));
 
         if(args.length < 2){
             System.err.println("请输入正确格式的参数：\n参数1：联系人路径 \n参数2：通话记录输出路径");
